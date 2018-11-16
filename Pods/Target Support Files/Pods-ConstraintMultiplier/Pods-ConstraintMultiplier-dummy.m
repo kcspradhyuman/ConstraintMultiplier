889ccd51e589f2885dc1cad229cd1c293adea447
pradhyuman <pradhyuman@mac-mini-2.local>
2018-11-16 10:17:01 +0530

    fastlane

---
+++ Pods/Target Support Files/Pods-ConstraintMultiplier/Pods-ConstraintMultiplier-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ConstraintMultiplier : NSObject
+@end
+@implementation PodsDummy_Pods_ConstraintMultiplier
+@end
---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "com.kcspl.poc" } // The bundle identifier of your app
+var appleID: String { return "info@kcspl.co.in" } // Your Apple email address
+
+var itcTeam: String? { return "623045" } // App Store Connect Team ID
+var teamID: String { return "GWX9XCPYYK" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
